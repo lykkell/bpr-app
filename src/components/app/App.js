4d3c713d9373f0375ab4af7ec9e854d9354af981
lykkell <lykkellit@gmail.com>
2021-03-01 20:37:07 +0200

    add main dashboard

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+// Dashboard
+import logo from './logo.jpg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
+      <div className="Dash-board-main" alt="Main dashboard">
+        <div className="Dash-board-btn-up">
+          <btn classsName="Likar-Dash-board">Likar</btn>
+          <btn classsName="Training-School-Dash-board">Training-School</btn>
+        </div>
+        <div className="Dash-board-btn-middle">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="Dash-board-btn-down">
+          <btn classsName="Certifying-commission-Dash-board">Certifying-commission</btn>
+          <btn classsName="Seminars-Dash-board">Seminars</btn>
+        </div>
+        <div className="News-feed" alt="News feed">
+          News feed
+        </div>
+        <div className="Adv" alt="ad block">
+          advertisement
+        </div>
+      </div>
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        
+      
       </header>
     </div>
   );
