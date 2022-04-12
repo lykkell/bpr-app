6515bca48eaf6ee52f827dcd15065d415ea3a660
lykkell <lykkellit@gmail.com>
2022-04-12 13:55:03 +0200

    add login validatoin

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 // Dashboard
-// import React from 'react';
 import React, { useState } from "react";
+import {useEffect} from 'react';
 import logo from './logo.jpg';
 import accountpicture from './accountpicture.jpg';
 import { useTranslation } from "react-i18next";
@@ -9,6 +9,86 @@ import i18n from "i18next";
 import './App.css';
 import Modal from '../modal';
 import AccountCard from "../account-card";
+// import { useEffect } from "react/cjs/react.production.min";
+
+const useValidation = (value, validations) => {
+    const [isEmpty, setEmpty] = useState(true)
+    const [minLengthError, setMinLengthError] = useState(false)
+    const [maxLengthError, setMaxLengthError] = useState(false)
+    const [emailError, setEmailError] = useState (true)
+    const [inputValid, setInputValid] = useState (false)
+
+  useEffect( () => {
+    for (const validation in validations) {
+      switch (validation) {
+
+        case 'minLength':
+          value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false)
+        break
+
+        case 'maxLenght':
+          value.length > validations[validation] ? setMaxLengthError(true) : setMaxLengthError(false)
+        break
+
+        case 'isEmpty' :
+          value ? setEmpty(false) : setEmpty(true)
+        break
+        
+        case 'isEmail' :
+          // eslint-disable-next-line no-useless-escape
+          const res = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          res.test(String(value).toLowerCase()) ? setEmailError (false) : setEmailError (true)
+          
+        break
+
+        default:
+        /* code */
+        break
+
+      }
+    }
+  },[validations, value])
+  
+  // useEffect( () => {
+
+  //     if (isEmpty || maxLengthError || minLengthError || emailError) {
+  //       setInputValid (false)
+  //     } else {
+  //       setInputValid(true)
+  //   }
+
+  // }, [isEmpty, maxLengthError, minLengthError, emailError])
+  
+  return {
+    isEmpty,
+    minLengthError, 
+    maxLengthError,
+    emailError,
+    // inputValid
+  }
+}
+
+const useImput = (initialValue, validations) => {
+
+    const [value, setValue] = useState(initialValue)
+    const [isDirty, SetDirty] = useState(false)
+    const valid = useValidation(value,validations)
+    const onChange = (e) => {
+      setValue(e.target.value)
+    }
+
+    const onBlur =(e) => {
+      SetDirty(true)
+    }
+
+    return {
+      value,
+      onChange,
+      onBlur,
+      isDirty,
+      ...valid
+    }
+}
 
 const App = () => {
   const { t } = useTranslation();
@@ -23,6 +103,8 @@ const App = () => {
   const [modalRegistrate, setModalRegistrate] = useState(false)
   const [modalForgetPassword, setModalForgetPassword] = useState(false)
   const [accountCard, setAccountCard] = useState(false)
+  const email = useImput('', {isEmpty: true, minLength: 7, maxLenght: 40, isEmail: true })
+  const password = useImput('', {isEmpty: true, minLength: 6, maxLenght: 16})
 
   return (
     
@@ -72,56 +154,59 @@ const App = () => {
         <div className="New-feed" alt="News feed">News feed</div>
         <div className="Adv" alt="ad block">advertisement</div>
       </div>
+      {/* Modal windows */}
+      {/* Validation account */}
       <Modal lang={language} active={modalLogin} setActive={setModalLogin}>
         <h2 className="modal__header">{t("Login")}</h2>
         <hr></hr>
         <form>
           <div> 
             <h3>{t("YourAccount")}:</h3>
-            <input 
-            className="modal__account"
-            type="login"
-            name="account"
-            placeholder="Enter your phone (XX) XXXXXXX or e-mail"
+            {(email.isDirty && email.isEmpty) && <div style={{color:'yellow'}}>Поле не повинне бути порожнім</div>}
+            {(email.isDirty && email.minLengthError) && <div style={{color:'yellow'}}>Недостатня довжина</div>}
+            {(email.isDirty && email.maxLengthError) && <div style={{color:'yellow'}}>Завелика довжина</div>}
+            {(email.isDirty && email.emailError) && <div style={{color:'yellow'}}>Email некорректий</div>}
+            <input onChange={e => email.onChange(e)} onBlur={e => email.onBlur(e)}value={email.value} className="modal__account" type="text" name="account" placeholder="Enter your phone (XX) XXXXXXX or e-mail"
             ></input>
             <h3>{t("Password")}</h3>
-            <input 
-            className="modal__account"
-            type="password"
-            name="password"
-            placeholder="Enter your password"
+            {(password.isDirty && password.isEmpty) && <div style={{color:'yellow'}}>Поле не повинне бути порожнім</div>}
+            {(password.isDirty && password.minLengthError) && <div style={{color:'yellow'}}>Слабкий пароль</div>}
+            {(password.isDirty && password.maxLengthError) && <div style={{color:'yellow'}}>long password</div>}
+            <input onChange={e => password.onChange(e)} onBlur={e => password.onBlur(e)}value={password.value} className="modal__account" type="password" name="password" placeholder="Enter your password"
             ></input>
           </div>  
           <div className='modal__footer'>
             <div className='modal__button'>
               {/* <button type="button" className="btn btn-warning">Clear</button> */}
-              <button 
-              type="submit"
-              className="btn btn-primary">
+              <button type="submit" className="btn btn-primary" 
+              disabled={password.isEmpty || password.maxLengthError || password.minLengthError || email.isEmpty || email.emailError} >
                 {t("Submit")}
               </button>
             </div>
             <div className='modal__button__short'>
-              <button 
-              className="modal__registrate btn btn-link"
-              onClick={() => setModalRegistrate(true) & setModalLogin(false) }
-              >{t("Registrate")}</button>
-              <button 
-              className="modal__forget__password btn btn-link"
-              onClick={() => setModalForgetPassword(true) & setModalLogin(false) }
-              >{t("Forget")}</button>
+              <button className="modal__registrate btn btn-link"
+                onClick={() => setModalRegistrate(true) & setModalLogin(false) }>
+                {t("Registrate")}
+              </button>
+              <button className="modal__forget__password btn btn-link"
+                onClick={() => setModalForgetPassword(true) & setModalLogin(false) }>
+                {t("Forget")}
+              </button>
             </div>
           </div>
         </form>
       </Modal>
+      {/* Registration new account */}
       <Modal lang={language} active={modalRegistrate} setActive={setModalRegistrate}>
         <h2 className="modal__header">Registrate new account</h2>
         <hr></hr>
       </Modal>
+      {/* Recovery account */}
       <Modal lang={language} active={modalForgetPassword} setActive={setModalForgetPassword}>
         <h2 className="modal__header">Forget my password</h2>
         <hr></hr>
       </Modal>
+      {/* Account windows */}
       <AccountCard lang={language} active={accountCard} setActive={setAccountCard}>
         <div className="container">
           <h2 className="account__card__header">{t("YourAccount")}</h2>
@@ -145,6 +230,8 @@ const App = () => {
               
             </div>
         </div>
+      
+      
       </AccountCard>
     </div>
   );
